c8d3adb06a4f21197780db8ebd7ca0986e8a6fb2
KonradBalon <konrad.balon@gmail.com>
2018-10-05 10:54:43 +0200

    v0.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void proba(int numerProby) {
@@ -24,13 +27,39 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        System.out.println("Witaj w grze Bulls & Cows!! \n wybierz 1 - Nowa Gra \n wybierz 2 - Zasady Gry \n wybierz 3 - Koniec");
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        choice = scanner.nextInt();
 
-        int licznikProb = 1;
-        proba(licznikProb);
+        switch (choice) {
 
 
-        //
-        //   System.out.println("To jest Twoja proba nr " + i);
+            case 1: {
+                int licznikProb = 1;
+                proba(licznikProb);
+                break;
+            }
+
+
+            case 2: {
+                System.out.println("ZASADY GRY\nKomputer (Host) losuje słowo, które jest izogramem.\n" +
+                        "(izogram jest to wyraz  w którym nie powtarzają się żadne litery) i informuje użytkownika (Guesser) o ilości liter  w słowie. \n" +
+                        "Użytkownik (Guesser) stara się zgadnąć co to za słowo. Komputer (Host) po każdej próbie zwraca liczbe Cows & Bulls. \n" +
+                        "Liczba przy słowie Cows oznacza literę występującą w słowie lecz na złej pozycji, liczba przy słowie Bulls oznacza poprawną literę na poprawnej pozycji. \n" +
+                        "Gra kończy się kiedy liczba przy Bulls będzie taka sama jak długość słowa wylosowanego przez komputer. \n");
+
+                System.out.println("Wcisnij enter, aby wrocic do menu");
+                break;
+            }
+            case 3:
+                System.out.println("KONIEC GRY");
+                break;
+
+
+            //  System.out.println("Nie masz wiecej prob!!!");
+
+
+        }
     }
-    //  System.out.println("Nie masz wiecej prob!!!");
 }
